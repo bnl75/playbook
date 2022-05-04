193c308afec37821b450762618e63719fdfc3ac4
bnl75 <diegoartu@hotmail.com>
2022-05-04 00:55:49 -0500

    Ejercicio 3 terminado

---
+++ weekly_mission_1/example3/main.js
@@ -0,0 +1,13 @@
+/*
+node main.js
+*/
+
+const logger1 = require('./logger_1')
+const logger2 = require('./logger_2')
+
+// console.log(logger1);
+// console.log(logger2);
+
+
+logger1('This is an informational message')
+logger2.verbose('This is a verbose message')
