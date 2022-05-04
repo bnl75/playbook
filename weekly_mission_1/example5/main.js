a68ad70c5baa0d2887bdd403b474716eb5a227a5
bnl75 <diegoartu@hotmail.com>
2022-05-04 01:15:15 -0500

    Ejercicio 5 terminado

---
+++ weekly_mission_1/example5/main.js
@@ -0,0 +1,16 @@
+const logger = require('./logger')
+
+// Ya se puede usar directamente el objeto instanciado en el módulo logger
+logger.log('This is an informational message')
+logger.log('This is a second informational message')
+
+console.log(logger.count);
+
+
+/*
+También pueder instanciar uno nuevo de esta manera:
+*/
+const customLogger = new logger.constructor('CUSTOM')
+customLogger.log('This is an informational message')
+
+console.log(customLogger.count);
