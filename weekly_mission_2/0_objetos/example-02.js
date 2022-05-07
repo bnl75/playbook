ef4275d25e777136ace8efd8987809c2a391925c
bnl75 <diegoartu@hotmail.com>
2022-05-06 21:56:06 -0500

    Ejercicio 2 terminado

---
+++ weekly_mission_2/0_objetos/example-02.js
@@ -0,0 +1,8 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObjetc2 = {
+    firstName: "Diego",
+    lastName: 'Bernal',
+    age: 23
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObjetc2)
