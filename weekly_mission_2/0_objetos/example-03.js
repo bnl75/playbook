cfdccd2ea42d0dfa752b3124e9b5a92f8f86ca40
bnl75 <diegoartu@hotmail.com>
2022-05-06 22:06:59 -0500

    Ejercicio 3 terminado

---
+++ weekly_mission_2/0_objetos/example-03.js
@@ -0,0 +1,23 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Kevin",
+    age: 5,
+    nicknames: [
+        "Kevinator",
+        "Furioso",
+        "kevinsin",
+        "kevincito",
+    ],
+    address: {
+        zip_code: "03000",
+        street: "Benito Juárez",
+        city: "Ciudad de México"
+    }
+}
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myObject3)
+console.log(myObject3.name)
+console.log(myObject3["address"])
+console.log(myObject3.address)
+console.log(myObject3.nicknames)
+console.log(myObject3["age"])
