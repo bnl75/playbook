809d320053425b94690fde13e126d389113164e2
bnl75 <diegoartu@hotmail.com>
2022-05-07 20:02:50 -0500

    Ejercicio 09 terminado

---
+++ weekly_mission_2/2_clases_y_objetos/example-09.js
@@ -0,0 +1,33 @@
+// Ejemplo 9: Herencia entre dos clases
+/*
+    HERENCIA: Nos permite relacionar clases entre sí y rehusar sus componentes.
+*/
+class Developer {
+    constructor(name, mainLang, stack){
+        this.name =  name
+        this.mainLang = mainLang
+        this.stack = stack
+    }
+    
+    get getName() {
+        return this.name
+    }
+}
+
+console.log("Ejemplo 9: Herencia entre dos clases")
+const diegoDev = new Developer("Diego Bernal", "JS", ["Elixir", "Groovy", "Lisp"])
+console.log(diegoDev)
+
+/* 
+    Se usa la palabra 'extends' para indicar que heredarás las propiedades de la
+    clase Padre (Developer) en la clase definida.
+*/
+
+// Podemos definir una clase vacía y rehusar todos los componentes de la clase padre:
+class LaunchXStudent extends Developer {
+}
+
+const diegoLaunchXDev = new LaunchXStudent("Diego Bernal", "JS", ["Elixir", "Groovy", "Lisp"])
+console.log(diegoLaunchXDev)
+console.log(diegoLaunchXDev.getName) // getter de la clase padre rehusada en la clase hija
+
